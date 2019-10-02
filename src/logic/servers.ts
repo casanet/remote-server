66b1d200a8ec60426f67be60f4602155bcb425f9
haim kastner <haim.kastner@gmail.com>
2019-10-02 15:54:52 +0300

    Wait before sending status notification #3 Case the status quickly returning to his original status.

--- src/logic/servers.ts
+++ src/logic/servers.ts
@@ -2,10 +2,16 @@ import { Channels, ChannelsSingleton } from './channels';
 import { getServer } from '../data-access';
 import { SendStatusNotificationMail } from '../mailSender';
 import { logger } from '../logger';
+import * as moment from 'moment';
+
 /**
  * Cache utility. wrap cache API to replace cache tool with redis client easily.
  */
 export class LocalServers {
+
+    private NOTIFICATION_TIME_WINDOW: moment.Duration = moment.duration(2, 'minutes');
+
+    private notificationTasks: { [key: string]: { status: boolean, notificationTask: NodeJS.Timeout, } } = {};
 
     constructor(private ChannelsSingleton: Channels) {
         this.ChannelsSingleton.localServersStautsFeed.subscribe((serverStatus) => {
@@ -18,22 +24,52 @@ export class LocalServers {
     }
 
     private async handleServerConnectionStatusChanged(serverStatus: { localServerId: string, theNewStatus: boolean }) {
-        
+
+        const lastNotificationTask = this.notificationTasks[serverStatus.localServerId];
+
+        /** If status same as the last notification, ignore sending another notification */
+        if (lastNotificationTask && lastNotificationTask.status === serverStatus.theNewStatus) {
+            return;
+        }
+
         /** Get the local server */
         const server = await getServer(serverStatus.localServerId);
 
         /** If there is no contact, abort. */
-        if(!server.contactMail){
+        if (!server.contactMail) {
             return;
         }
 
-        /** Send notification to the contact about the new status */
-        try {
-            await SendStatusNotificationMail(server.contactMail, server.displayName, serverStatus.theNewStatus)            
-            logger.info(`Email status '${serverStatus.theNewStatus}' notification of ${server.displayName} successfully sent`);
-        } catch (error) {
-            logger.error(`Sending mail notification failed, API problem, ${error.message}`);
+        /** 
+         * If the status changed back to original status in the notification time window, 
+         * cancel the notification task.
+         */
+        if (lastNotificationTask && lastNotificationTask.status !== serverStatus.theNewStatus) {
+            this.notificationTasks[serverStatus.localServerId] = null;
+            clearTimeout(lastNotificationTask.notificationTask);
+            return;
         }
+
+        /** 
+         * Hold the notification in a time window before sending, 
+         * case the status return to his original status
+         */
+        const timeoutTaskRef = setTimeout(async () => {
+            /** Send notification to the contact about the new status */
+            try {
+                await SendStatusNotificationMail(server.contactMail, server.displayName, serverStatus.theNewStatus)
+                logger.info(`Email status '${serverStatus.theNewStatus}' notification of ${server.displayName} successfully sent`);
+            } catch (error) {
+                logger.error(`Sending mail notification failed, API problem, ${error.message}`);
+            }
+            this.notificationTasks[serverStatus.localServerId] = null;
+        }, this.NOTIFICATION_TIME_WINDOW.asMilliseconds());
+
+        this.notificationTasks[serverStatus.localServerId] = {
+            notificationTask :timeoutTaskRef,
+            status :  serverStatus.theNewStatus,
+        }
+
     }
 
 }
